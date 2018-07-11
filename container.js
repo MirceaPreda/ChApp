9f1ceb0fe2fe7b7875d8bb1c033f53d01c30a79d
MirceaPreda <nito374@gmail.com>
2018-07-11 14:55:15 +0300

    Add files via upload

---
+++ container.js
@@ -0,0 +1,27 @@
+const dependable = require('dependable');
+const path = require('path');
+
+const container = dependable.container();
+
+const simpleDependecies = [
+    ['_', 'lodash'],
+    ['async', 'async'],
+    ['mongoose', 'mongoose'],
+    ['passport', 'passport'],
+    ['formidable', 'formidable']
+];
+
+simpleDependecies.forEach(function(val) {
+    container.register(val[0], function (){
+        return require(val[1]);
+    })
+});
+
+container.load(path.join(__dirname, '/controllers'));
+container.load(path.join(__dirname, '/helpers'));
+
+container.register('container', function(){
+    return container;
+});
+
+module.exports = container;
